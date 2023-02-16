8b0832e4ba920077fbc58d50ce3abeca2673b210
YunSukHyun <czcz9207@gmail.com>
2023-02-17 01:20:05 +0900

    delete read

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,35 +1,16 @@
-import { collection, getDocs } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { db, login, logout, onUserStateChange } from '../../service/firebase';
+import { login, logout, onUserStateChange } from '../../service/firebase';
 import Header from '../header/header';
 import User from '../user/user';
 import styles from "./home.module.css";
 
 const Home = () => {
-  const userCollectionRef = collection(db, "users");
-  const [votedArr, setVotedArr] = useState([]);
   const [user, setUser] = useState();
   
   useEffect(() => {
     onUserStateChange(setUser);
-    const getData = async () => {
-      const data = await getDocs(userCollectionRef);
-      let gc = 0, pc = 0;
-      const voted = data.docs;
-      voted.forEach(doc => {
-        const tmp = doc.data()
-        if(tmp.genshin){
-          gc++;
-        }
-        if(tmp.priconne){
-          pc++;
-        }
-      })
-      setVotedArr([gc, pc]);
-    }
-    getData();
-  }, [userCollectionRef]);
+  }, []);
 
   return (
     <>
@@ -46,8 +27,6 @@ const Home = () => {
         {user && <User user={user}></User>}
         {!user && <button className={styles.sign} onClick={login}>Login with Google</button>}
         {user && <button className={styles.sign} onClick={logout}>Logout</button>}
-        <h1>원신 투표수: {votedArr[0]}</h1>
-        <h1>프리코네 투표수: {votedArr[1]}</h1>
       </div>
      </>
   )
