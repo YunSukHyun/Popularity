47bbe77e1e93ee73ffec6b5e54ac8675b3eb420f
YunSukHyun <czcz9207@gmail.com>
2023-02-28 23:20:58 +0900

    vote finished

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,35 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { login, logout, onUserStateChange } from '../../service/firebase';
-import Header from '../header/header';
-import User from '../user/user';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { login, logout, onUserStateChange } from "../../service/firebase";
+import Header from "../header/header";
+import User from "../user/user";
 import styles from "./home.module.css";
 
 const Home = () => {
   const [user, setUser] = useState();
-  
+
   useEffect(() => {
     onUserStateChange(setUser);
   }, []);
 
   return (
     <>
-      <Header title='인기투표'/>
+      <Header title="인기투표(종료)" />
       <div className={styles.img}>
         <Link to="/genshin">
-          <img className={styles.genshin} src="image/link_img/genshin.png" alt="genshin"/>
+          <img
+            className={styles.genshin}
+            src="image/link_img/genshin.png"
+            alt="genshin"
+          />
         </Link>
         <Link to="/priconne">
-          <img className={styles.priconne} src="image/link_img/priconne.png" alt="priconne"/>
+          <img
+            className={styles.priconne}
+            src="image/link_img/priconne.png"
+            alt="priconne"
+          />
         </Link>
       </div>
       <div className={styles.user}>
         {user && <User user={user}></User>}
-        {!user && <button className={styles.sign} onClick={login}>Login with Google</button>}
-        {user && <button className={styles.sign} onClick={logout}>Logout</button>}
+        {!user && (
+          <button className={styles.sign} onClick={login}>
+            Login with Google
+          </button>
+        )}
+        {user && (
+          <button className={styles.sign} onClick={logout}>
+            Logout
+          </button>
+        )}
       </div>
-     </>
-  )
+    </>
+  );
 };
 
 export default Home;
