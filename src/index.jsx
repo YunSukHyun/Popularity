a336617ebd4fdd87ff0e3d0e482ce6a7d61101fe
YunSukHyun <czcz9207@gmail.com>
2023-12-17 20:52:47 +0900

    fix: remove strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,12 @@
 import App from "./app";
 import store from "./store";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
